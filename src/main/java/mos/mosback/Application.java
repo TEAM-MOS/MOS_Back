d243b5ec4e4eed3604ead4cc706484448a9e5f07
leejjeonghui <gdjh0513@naver.com>
2023-08-22 02:33:54 +0900

    엔티티클래스 따로 만들기 , CRUD, 검색 / 인기순조회

--- src/main/java/mos/mosback/Application.java
+++ src/main/java/mos/mosback/Application.java
@@ -3,7 +3,8 @@ package mos.mosback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
