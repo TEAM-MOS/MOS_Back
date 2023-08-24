2f426fbcee5f89b904b6685ad34212cc58458963
leejjeonghui <gdjh0513@naver.com>
2023-08-25 05:44:15 +0900

    컨트롤러, 필드 수정

--- src/main/java/mos/mosback/repository/StudyGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package mos.mosback.repository;
-
-
-import mos.mosback.entity.StudyGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
-    // Custom query methods, if needed
-}
