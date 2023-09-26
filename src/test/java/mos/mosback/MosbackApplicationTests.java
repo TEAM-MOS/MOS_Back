565e4de4e13fa065cd5ba7a6ad95dd8390c8b7cf
slonue <127409712+slonue@users.noreply.github.com>
2023-09-27 01:15:40 +0900

    Update MosbackApplicationTests.java

--- src/test/java/mos/mosback/MosbackApplicationTests.java
+++ src/test/java/mos/mosback/MosbackApplicationTests.java
@@ -3,7 +3,7 @@ package mos.mosback;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MosbackApplicationTests {
 
     @Test
