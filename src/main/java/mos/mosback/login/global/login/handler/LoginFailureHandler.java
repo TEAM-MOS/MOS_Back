710ece8678789880e08eacec3e3b791caf154c98
slonue <127409712+slonue@users.noreply.github.com>
2023-10-19 17:32:15 +0900

    Update LoginFailureHandler.java

--- src/main/java/mos/mosback/login/global/login/handler/LoginFailureHandler.java
+++ src/main/java/mos/mosback/login/global/login/handler/LoginFailureHandler.java
@@ -1,5 +1,6 @@
 package mos.mosback.login.global.login.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -7,6 +8,8 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationFa
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * JWT 로그인 실패 시 처리하는 핸들러
@@ -21,7 +24,14 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/plain;charset=UTF-8");
-        response.getWriter().write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.");
         log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
+
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("status", 200);
+        responseData.put("success", false);
+        responseData.put("메시지" , "로그인실패! 이메일이나 비밀번호를 확인해주세요");
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(responseData));
     }
 }
