2600d3ade938001aa59d48f5ca6a64c56f646ba1
slonue <127409712+slonue@users.noreply.github.com>
2023-10-19 17:32:01 +0900

    Update LoginSuccessHandler.java

--- src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package mos.mosback.login.global.login.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mos.mosback.login.domain.user.repository.UserRepository;
@@ -11,6 +12,9 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,15 +23,18 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtService jwtService;
     private final UserRepository userRepository;
 
+
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) {
+                                        Authentication authentication) throws IOException {
         String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
         String accessToken = jwtService.createAccessToken(email); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
+
+
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
 
@@ -40,7 +47,20 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         log.info("로그인에 성공하였습니다. 이메일 : {}", currentEmail);
         log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
         log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
+
+
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("status", 200);
+        responseData.put("success", true);
+        responseData.put("current email : {}", currentEmail);
+        responseData.put("AccessToken : ", accessToken);
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(responseData));
     }
+
+
 
     private String extractUsername(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
