6f4e6d4048107e98ba04aad128eec8d896c2ca6e
slonue <127409712+slonue@users.noreply.github.com>
2023-10-28 16:41:24 +0900

    Update UserProfileController.java

--- src/main/java/mos/mosback/login/domain/user/controller/UserProfileController.java
+++ src/main/java/mos/mosback/login/domain/user/controller/UserProfileController.java
@@ -1,9 +1,12 @@
 package mos.mosback.login.domain.user.controller;
 
+import mos.mosback.login.domain.user.User;
 import mos.mosback.login.domain.user.dto.UserProfileDto;
 import mos.mosback.login.domain.user.repository.UserRepository;
 import mos.mosback.login.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +33,6 @@ public class UserProfileController {
         this.userRepository = userRepository;
 
     }
-
 
     @PostMapping
     public Map<String, Object> createUserProfile(@RequestBody UserProfileDto userProfileDto) {
@@ -76,5 +78,33 @@ public class UserProfileController {
             }
 
     return response;
-}}
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getUserProfile() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            // 현재 로그인한 사용자의 정보 가져오기
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String currentEmail = authentication.getName(); // 현재 사용자의 이메일
+
+            // 현재 사용자의 이메일을 이용하여 프로필 정보를 가져옵니다.
+            UserProfileDto userProfileDto = userService.getUserProfileByEmail(currentEmail);
+
+            response.put("status", 200);
+            response.put("success", true);
+            response.put("data", userProfileDto);
+
+            // 프로필 정보를 클라이언트에 응답으로 반환합니다.
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // 에러 발생 시 500 Internal Server Error 반환
+            response.put("status", 500);
+            response.put("success", false);
+            response.put("message", "오류가 발생했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+}
 
