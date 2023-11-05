720c804c29907ecf694e044ce31b314cce4081cc
slonue <127409712+slonue@users.noreply.github.com>
2023-11-05 15:38:46 +0900

    Update LoginSuccessHandler.java

--- src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
@@ -3,12 +3,14 @@ package mos.mosback.login.global.login.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mos.mosback.login.domain.user.dto.UserInfo;
 import mos.mosback.login.domain.user.repository.UserRepository;
 import mos.mosback.login.global.jwt.service.JwtService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,11 +30,10 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private String accessTokenExpiration;
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException {
+                                         Authentication authentication) throws IOException {
         String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
         String jwt = jwtService.createJwt(email); // JwtService의 createJwt를 사용하여 Jwt 토큰 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
-
         jwtService.sendAccessAndRefreshToken(response, jwt, refreshToken); // 응답 헤더에 Jwt, RefreshToken 실어서 응답
 
         String currentEmail = email;
@@ -45,11 +46,14 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         log.info("로그인에 성공하였습니다. Jwt : {}", jwt);
         log.info("발급된 Jwt 만료 기간 : {}", accessTokenExpiration);
 
+        Map<String, Object> result = new HashMap<>();
+        result.put("currentEmail", currentEmail); // 사용자 고유 식별자 (예: DB에서 조회한 값)
+        result.put("jwt", jwt);
+
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("status", 200);
         responseData.put("success", true);
-        responseData.put("currentEmail", currentEmail);
-        responseData.put("jwt", jwt);
+        responseData.put("result", result);
 
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
