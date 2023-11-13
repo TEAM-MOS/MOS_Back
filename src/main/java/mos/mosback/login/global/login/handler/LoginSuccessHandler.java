4cc262974a6a57a068432c847884f02df3971a12
slonue <127409712+slonue@users.noreply.github.com>
2023-11-13 18:26:26 +0900

    Update LoginSuccessHandler.java

--- src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/mos/mosback/login/global/login/handler/LoginSuccessHandler.java
@@ -29,25 +29,25 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
 
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                          Authentication authentication) throws IOException {
         String email = extractUsername(authentication); // 인증 정보에서 Username(email) 추출
         String jwt = jwtService.createJwt(email); // JwtService의 createJwt를 사용하여 Jwt 토큰 발급
         String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
         jwtService.sendAccessAndRefreshToken(response, jwt, refreshToken); // 응답 헤더에 Jwt, RefreshToken 실어서 응답
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+        Role role = user.getRole();
+
 
         String currentEmail = email;
-        userRepository.findByEmail(currentEmail)
-                .ifPresent(user -> {
-                    user.updateRefreshToken(refreshToken);
-                    userRepository.saveAndFlush(user);
-                });
+
         log.info("로그인에 성공하였습니다. 이메일 : {}", currentEmail);
         log.info("로그인에 성공하였습니다. Jwt : {}", jwt);
         log.info("발급된 Jwt 만료 기간 : {}", accessTokenExpiration);
 
         Map<String, Object> result = new HashMap<>();
         result.put("jwt", jwt);
+        result.put("role", role);
 
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("status", 200);
@@ -58,7 +58,6 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().write(new ObjectMapper().writeValueAsString(responseData));
     }
-
 
 
     private String extractUsername(Authentication authentication) {
